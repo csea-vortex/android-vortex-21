ec7bbce66e8ad2ce7bc6fe276fb71a0417b0d9b3
Aditya Kumar <k.aditya00@gmail.com>
2020-09-22 07:31:35 +0530

    Initial Commit
    Setup android project with
    - Fragment + ViewModel Template
    - Android 5.0 >= support
    - Kotlin based

---
+++ app/src/main/java/edu/nitt/vortex/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.nitt.vortex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.nitt.vortex.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/edu/nitt/vortex/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package edu.nitt.vortex.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
