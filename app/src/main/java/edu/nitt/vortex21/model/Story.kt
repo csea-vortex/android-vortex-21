5e53c5895d30bce2efc4dbc9c94609f1a6c223ae
avikumar15 <avithewinner1508@gmail.com>
2020-11-07 11:41:33 +0530

    refactor: refactor story data flow

--- app/src/main/java/edu/nitt/vortex21/model/Story.kt
+++ app/src/main/java/edu/nitt/vortex21/model/Story.kt
@@ -1,7 +1,11 @@
 package edu.nitt.vortex21.model
 
-class Story (
-    public var imageurl:String = "",
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Story(
+    public var imageurl:List<String>,
     public var storyid:String = "",
     public var storyName:String = ""
-)
+):Parcelable
