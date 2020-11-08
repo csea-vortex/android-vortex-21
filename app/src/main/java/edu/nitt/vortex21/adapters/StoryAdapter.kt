644a95ce0e72cc6a784f6cca9c8f4bbc02d02d8b
avikumar15 <avithewinner1508@gmail.com>
2020-11-08 10:39:38 +0530

    refactor: code cleanup

--- app/src/main/java/edu/nitt/vortex21/adapters/StoryAdapter.kt
+++ app/src/main/java/edu/nitt/vortex21/adapters/StoryAdapter.kt
@@ -1,6 +1,5 @@
 package edu.nitt.vortex21.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,36 +7,28 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.hdodenhof.circleimageview.CircleImageView
 import edu.nitt.vortex21.R
+import edu.nitt.vortex21.databinding.StoryItemBinding
 import edu.nitt.vortex21.model.Story
 import kotlinx.android.synthetic.main.story_item.view.*
 
 class StoryAdapter(
-    private val mContext: Context,
     private var stories: List<Story>,
-    private val clickListener:(Int)->Unit
-    ) : RecyclerView.Adapter<StoryAdapter.StoryViewHolder>()
-{
+    private val clickListener: (Int) -> Unit
+) : RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
 
-    class StoryViewHolder(val view: View): RecyclerView.ViewHolder(view){
-        var storyImageSeen = view.findViewById(R.id.story_image_seen) as CircleImageView
-        var storyImage = view.findViewById(R.id.story_image) as CircleImageView
-        var storyName = view.findViewById(R.id.story_name) as TextView
+    class StoryViewHolder(val binding: StoryItemBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(index: Int, story: Story, clickListener: (Int)->Unit){
-            view.story_name.text = story.storyName
-            view.setOnClickListener{
+            binding.storyName.text = story.storyName
+            binding.root.setOnClickListener{
                 clickListener(index)
             }
-
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.story_item, parent, false)
-
-        return StoryViewHolder(view)
+        val binding = StoryItemBinding.inflate(LayoutInflater
+            .from(parent.context),parent,false)
+        return StoryViewHolder(binding)
     }
 
     override fun getItemCount() = stories.size
