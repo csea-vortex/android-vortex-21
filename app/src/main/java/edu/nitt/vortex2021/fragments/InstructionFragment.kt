27a3f9ea10cc503b6d6ea5e042f7361bfa78af97
Pradeep S <pradeep.deep7@gmail.com>
2020-12-26 21:46:04 +0530

    feat: add navigation to linked fragment

--- app/src/main/java/edu/nitt/vortex2021/fragments/InstructionFragment.kt
+++ app/src/main/java/edu/nitt/vortex2021/fragments/InstructionFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import edu.nitt.vortex2021.R
 import edu.nitt.vortex2021.databinding.FragmentEventsBinding
 import edu.nitt.vortex2021.databinding.FragmentInstructionBinding
@@ -32,7 +33,7 @@ class InstructionFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.textView.movementMethod = ScrollingMovementMethod()
         binding.continueButton.setOnClickListener {
-            // navigate to game fragment
+            findNavController().navigate(InstructionFragmentDirections.actionInstructionFragmentToLinkedFragment())
         }
     }
 
