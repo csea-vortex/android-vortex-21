9c742e59a0d4fc3fe36ee98bd4b166d455eb859a
Aditya Kumar <k.aditya00@gmail.com>
2020-12-28 09:29:16 +0530

    feat(workshops): viewmodel integration

---
+++ app/src/main/java/edu/nitt/vortex2021/adapters/WorkshopAdapter.kt
@@ -0,0 +1,87 @@
+package edu.nitt.vortex2021.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import edu.nitt.vortex2021.databinding.ListItemWorkshopBinding
+import edu.nitt.vortex2021.helpers.Constants
+import edu.nitt.vortex2021.helpers.getFormatted
+import edu.nitt.vortex2021.model.Workshop
+
+class WorkshopAdapter(
+    private val currentUserEmail: String,
+    private val workshops: List<Workshop>,
+    private val onRegisterButtonClickListener: (workshop: Workshop) -> Unit
+) : RecyclerView.Adapter<WorkshopAdapter.WorkshopViewHolder>() {
+
+    inner class WorkshopViewHolder(val binding: ListItemWorkshopBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(workshop: Workshop) {
+            binding.workshopName.text = workshop.title
+            binding.workshopFrom.text = "\uD83D\uDCC5 Starts ${workshop.eventFrom.getFormatted()}"
+            binding.workshopTo.text = "\uD83D\uDCC5 Ends ${workshop.eventTo.getFormatted()}"
+
+            if (workshop.isFree || (currentUserEmail.endsWith("@nitt.edu") && workshop.isNITTFree)) {
+                binding.workshopCost.text = "\uD83D\uDCB0 Free"
+            } else {
+                binding.workshopCost.text = "\uD83D\uDCB0 ₹${workshop.cost}"
+            }
+
+            var description = workshop.description + "\n\n"
+            description += "Instructions: \n"
+            for (instructionPoint in workshop.instructions) {
+                description += "• $instructionPoint\n"
+            }
+
+            binding.description.text = description
+
+            try {
+                Picasso.get().load(Constants.BACKEND_BASE_URL + workshop.smallImage)
+                    .into(binding.workshopLogo, object : Callback {
+                        override fun onSuccess() = Unit
+                        override fun onError(e: Exception?) {
+                            e?.stackTrace
+                        }
+                    })
+            } catch (e: Exception) {
+                e.stackTrace
+            }
+
+            binding.registerButton.apply {
+                isEnabled = System.currentTimeMillis() < workshop.eventTo.time
+                setOnClickListener { onRegisterButtonClickListener(workshop) }
+            }
+
+            binding.detailsButton.setOnClickListener {
+                binding.description.apply {
+                    visibility = if (visibility == View.GONE) {
+                        View.VISIBLE
+                    } else {
+                        View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkshopViewHolder {
+        val binding = ListItemWorkshopBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return WorkshopViewHolder(binding)
+    }
+
+    override fun getItemCount() = workshops.size
+
+    override fun onBindViewHolder(holder: WorkshopViewHolder, position: Int) {
+        holder.bind(workshops[position])
+    }
+
+}
--- app/src/main/java/edu/nitt/vortex2021/fragments/WorkshopsFragment.kt
+++ app/src/main/java/edu/nitt/vortex2021/fragments/WorkshopsFragment.kt
@@ -1,29 +1,86 @@
 package edu.nitt.vortex2021.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import edu.nitt.vortex2021.BaseApplication
 import edu.nitt.vortex2021.R
+import edu.nitt.vortex2021.adapters.WorkshopAdapter
 import edu.nitt.vortex2021.databinding.FragmentWorkshopsBinding
-import edu.nitt.vortex2021.helpers.viewLifecycle
+import edu.nitt.vortex2021.helpers.*
+import edu.nitt.vortex2021.model.Workshop
+import edu.nitt.vortex2021.viewmodel.DataViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class WorkshopsFragment : Fragment() {
 
     private var binding by viewLifecycle<FragmentWorkshopsBinding>()
 
+    private lateinit var dataViewModel: DataViewModel
+    private val workshops = ArrayList<Workshop>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentWorkshopsBinding.inflate(inflater, container, false)
+        initViewModels()
+        initGradientBackgroundAnimation(binding.root)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().setTitle(R.string.workshops)
+
+        initRecyclerViews()
+        dataViewModel.fetchWorkshops()
+    }
+
+    private fun initRecyclerViews() {
+        val userStore = UserSharedPrefStore(requireContext())
+        binding.workshopsList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = WorkshopAdapter(userStore.email, workshops) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it.link))
+                requireContext().startActivity(intent)
+            }
+        }
+    }
+
+
+    private fun initViewModels() {
+        val factory = (requireActivity().application as BaseApplication)
+            .applicationComponent
+            .getViewModelProviderFactory()
+
+        dataViewModel = ViewModelProvider(this, factory).get(DataViewModel::class.java)
+        observeLiveData()
+    }
+
+
+    private fun observeLiveData() {
+        dataViewModel.workshopsResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> {
+                    val data = response.data!!.workshops
+                    workshops.clear()
+                    workshops.addAll(data)
+                    binding.workshopsList.adapter?.notifyDataSetChanged()
+                }
+                is Resource.Error -> {
+                    showToastMessage(requireContext(), response.message)
+                }
+            }
+        }
     }
 }
